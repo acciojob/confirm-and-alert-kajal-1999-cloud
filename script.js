10c7f6d8e558838a993e320b5f8dadf916bf8a1d
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-26 16:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If 
+
+const  isConfirmed = confrm("Do you want to proceed?")
+
+if(isConfirmed){
+	alert("You clicked Ok. Proceeding...")
+}
+else{
+	alert("You clicked cancel. Exiting...")
+}
